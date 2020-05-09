5ffca97c66eab6d81c7800fbea6f59628a8d02f6
Hollowstare <Magickpen@yandex.ru>
2020-05-09 16:58:20 +0800

    Add clearText function

--- src/textWork.cpp
+++ src/textWork.cpp
@@ -24,3 +24,29 @@ void inputText()
 
     write.close();
 }
+
+void clearText(string readFile, string writeFile)
+{
+	ifstream read;
+	ofstream write;
+	read.open(readFile);
+	write.open(writeFile);
+
+	string word;
+	while (read >> word) {
+		string symb = ".()-:!?[]*%;#№@$&+=_";
+		while (word.rfind(",") != 4294967295
+			&& word.rfind(",") == word.length() - 1) {
+			word = word.erase(word.rfind(","), 1);
+		}
+		for (int i = 0; i < 22; i++) {
+			while (word.rfind(symb[i]) != 4294967295) {
+				word = word.erase(word.rfind(symb[i]), 1);
+			}
+		}
+
+		write << word << " ";
+	}
+	cout << endl << endl;
+	write.close();
+}
