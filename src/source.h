80ba217b14e12790b4c0249330ec4bcc7f0bbf65
Hollowstare <Magickpen@yandex.ru>
2020-05-26 22:51:16 +0800

    Edit function prototypes

--- src/source.h
+++ src/source.h
@@ -6,6 +6,6 @@ using namespace std;
 unordered_set<string> introduceDictionary(string filename);
 string excessLetter(string word, unordered_set<string> Dictionary);
 void checkText(string filename, unordered_set<string> Dictionary);
-string missingLetter(string word, unordered_set<string> Dictionary);
+string missingLetter(string word, unordered_set<string> Dictionary, string& text);
 string swapLetters(string word, unordered_set<string> Dictionary);
-string wrongLetter(string word, unordered_set<string> Dictionary);
+string wrongLetter(string word, unordered_set<string> Dictionary, string& text);
